8c14b13c6873855747713a0a7227fd2de9050d11
harrikr <kurniawanharry165@gmail.com>
2017-06-04 05:52:28 +0700

    Assigment2
    Details on Readme.md

--- semester6/angular-ngspotify/src/app/komponen/Tentang/tentang.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId:module.id,
-  selector: 'tentang',
-  templateUrl: `tentang.component.html`
-})
-export class TentangComponent  {}
